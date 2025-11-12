9c2aff6f91d29d5f9610f1666b6aca7d1132af95
Shivam <shivamtamboli66@gmail.com>
2025-11-12 11:11:52 +0530

    update loadingspinner

---
+++ frontend/src/components/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/LoadingSpinner.css';
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
